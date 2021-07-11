bb6e74e062b4126666f3c505b5d0f6b4b51914e7
ekaterina <besputova@gmail.com>
2021-07-11 16:52:44 +0300

    create bgColor change code by timer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-import './sass/main.scss';
+import colors from './bg-colors';
+import { randomIntegerFromInterval } from './choose-color';
+
+const startBgColorChangeBtn = document.querySelector('[data-action="start"]');
+const stopBgColorChangeBtn = document.querySelector('[data-action="stop"]');
+const bodyElement = document.body;
+let timerSetElementRandomBgColor = null;
+
+const changeElementBgColor = (element, bgColor) => {
+  let elem = element;
+  if (!elem instanceof HTMLElement) {
+    elem = document.querySelector(elem);
+  }
+  if (elem) {
+    element.style.backgroundColor = bgColor;
+  }
+};
+
+const setElementRandomBgColor = element => {
+  const index = randomIntegerFromInterval(0, colors.length - 1);
+  const newBgColor = colors[index];
+  changeElementBgColor(element, newBgColor);
+};
+
+const onStartBgColorChangeBtnClick = () => {
+  timerSetElementRandomBgColor = setInterval(setElementRandomBgColor, 1000, bodyElement);
+  activateStopBgColorChangeBtn();
+};
+
+const activateStartBgColorChangeBtn = () => {
+  startBgColorChangeBtn.addEventListener('click', onStartBgColorChangeBtnClick, { once: true });
+};
+
+const onStopBgColorBtnClick = () => {
+  clearInterval(timerSetElementRandomBgColor);
+  activateStartBgColorChangeBtn();
+};
+
+const activateStopBgColorChangeBtn = () => {
+  stopBgColorChangeBtn.addEventListener('click', onStopBgColorBtnClick, { once: true });
+};
+
+activateStartBgColorChangeBtn();
